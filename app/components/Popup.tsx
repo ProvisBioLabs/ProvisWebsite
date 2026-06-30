2722cd357cd494de396f71391a5ab6841dd95c1f
vinay <thevinay02@gmail.com>
2026-06-30 14:29:59 +0530

    fix check

--- app/components/Popup.tsx
+++ app/components/Popup.tsx
@@ -54,7 +54,7 @@ export default function Popup() {
                   src="/events/biointernational.webp" 
                   alt="Upcoming Event" 
                   fill 
-                  className="object-cover transition-transform duration-700 group-hover:scale-105"
+                  className="object-fit transition-transform duration-700 group-hover:scale-105"
                   sizes="(max-width: 768px) 100vw, 600px"
                   priority
                 />
